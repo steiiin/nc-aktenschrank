1f34628e0c771d35483eafd29a2869608bc0a84a
steiiin <dev@steiiin.de>
2024-08-03 16:58:25 +0200

    add router.js

---
+++ src/router.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import { generateUrl } from '@nextcloud/router'
+import queryString from 'query-string'
+
+// const Timeline = () => import('./routes/Timeline')
+// const Inbox = () => import('./routes/Inbox')
+// const Archive = () => import('./routes/Archive')
+// const Settings = () => import('./routes/Settings')
+
+Vue.use(Router)
+export default new Router({
+
+  mode: 'history',
+  base: generateUrl('/apps/aktenschrank'),
+  linkActiveClass: 'active',
+  routes: [
+
+    // {
+    //   path: '/',
+    //   redirect: '/timeline',
+    // },
+    // {
+    //   path: '/timeline',
+    //   component: Timeline,
+    // },
+    // {
+    //   path: '/inbox',
+    //   component: Inbox,
+    // },
+    // {
+    //   path: '/archive',
+    //   component: Archive,
+    // },
+    // {
+    //   path: '/settings',
+    //   component: Settings,
+    // },
+
+  ],
+
+  // Custom stringifyQuery to prevent encoding of slashes in the url
+  stringifyQuery(query) {
+    const result = queryString.stringify(query).replace(/%2F/gmi, '/').replace(/%2C/gmi, ',')
+    return result ? ('?' + result) : ''
+  },
+
+})

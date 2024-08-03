963c8a10944ba035f44f9efe57f090e60142ede3
steiiin <dev@steiiin.de>
2024-08-03 17:00:09 +0200

    add router and pinia to app init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
+
+/* Pinia */
+import { createPinia, PiniaVuePlugin } from 'pinia'
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+
+/* Localization */
 Vue.mixin({ methods: { t, n } })
 
-const View = Vue.extend(App)
-new View().$mount('#aktenschrank')
+/* App */
+export default new Vue({
+	el: '#aktenschrank',
+	router,
+	pinia,
+	render: h => h(App),
+})

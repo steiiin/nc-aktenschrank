c6acdcbedc4d8f873082ac9fd736fdad2daee54a
steiiin <dev@steiiin.de>
2024-08-03 16:37:27 +0200

    restrict eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,8 @@ module.exports = {
 	rules: {
 		'jsdoc/require-jsdoc': 'off',
 		'vue/first-attribute-linebreak': 'off',
+		'vue/multiline-html-element-content-newline': 'off',
+		'vue/html-indent': 'off',
+		'indent': 'off'
 	},
 }

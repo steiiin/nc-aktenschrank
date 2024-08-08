676370f40993e7a8079d83e7c42ea0f9e13677a0
steiiin <dev@steiiin.de>
2024-08-08 14:04:07 +0200

    update eslint: unused-vars only warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,12 @@ module.exports = {
 	rules: {
 		'jsdoc/require-jsdoc': 'off',
 		'vue/first-attribute-linebreak': 'off',
+		'vue/max-attributes-per-line': 'off',
 		'vue/multiline-html-element-content-newline': 'off',
 		'vue/html-indent': 'off',
 		'indent': 'off',
-		'no-debugger': 'off',
+		'no-unused-vars': 'warn',
+		'no-debugger': 'off',	/* TODO: remove */
+		'no-console': 'off',	/* TODO: remove */
 	},
 }

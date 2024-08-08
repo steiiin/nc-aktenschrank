bba970d75f42ac2ef982e01df2f93571986e9441
steiiin <dev@steiiin.de>
2024-08-08 14:05:57 +0200

    add components: groupbox / input group

---
+++ src/components/FrameGroup.vue
@@ -0,0 +1,44 @@
+<!-- FrameGroup.vue -->
+<template>
+  <div class="aktn-group">
+    <div class="aktn-group--title">
+      {{ title }}
+    </div>
+    <div class="aktn-group--content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+
+/**
+ * Base component.
+ */
+export default {
+  name: 'FrameGroup',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+
+</script>
+<style scoped lang="scss">
+.aktn-group {
+
+  &--title {
+    background: var(--color-background-dark);
+    padding: .5rem;
+    font-weight: bold;
+    border-radius: var(--border-radius-small);
+  }
+
+  &--content {
+    padding: .5rem;
+  }
+
+}
+</style>
---
+++ src/components/FrameInput.vue
@@ -0,0 +1,65 @@
+<!-- FrameInput.vue -->
+<template>
+  <div class="aktn-input">
+    <label v-if="!!label && label.length > 0">
+      {{ label }}
+    </label>
+    <div class="aktn-input--wrapper">
+      <div class="aktn-input--control">
+        <slot name="control" />
+      </div>
+      <div v-if="hasActions" class="aktn-input--actions">
+        <slot name="actions" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+/**
+ * Base component.
+ */
+export default {
+  name: 'FrameInput',
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  computed: {
+    hasActions() {
+      return !!(this.$slots.actions || [])[0]
+    },
+  },
+}
+
+</script>
+<style scoped lang="scss">
+.aktn-input {
+
+  margin: 0.5rem 0;
+
+  & label {
+    display: block;
+    font-size: 0.8rem;
+    opacity: 0.75;
+    text-transform: uppercase;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  &--wrapper {
+    display: flex;
+    gap: .5rem;
+  }
+
+  &--control {
+    flex: 1;
+  }
+
+}
+</style>

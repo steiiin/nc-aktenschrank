a0e2404f9b98179a9e47fd668404cc06f81becc9
steiiin <dev@steiiin.de>
2024-08-18 12:38:01 +0200

    implement contentservice

---
+++ src/service/contentService.js
@@ -0,0 +1,17 @@
+// contentService.js
+
+class ContentService {
+
+  getMimeText(mimetype) {
+    return mimetype
+  }
+
+  async generatePreview(file) {
+    // Code to generate preview
+    console.log('Generating preview for', file.name)
+    // ... actual implementation
+  }
+
+}
+
+export default new ContentService()

21936750bb7d139ddd6219bf2b87bb003f523e6a
steiiin <dev@steiiin.de>
2024-08-18 12:37:53 +0200

    add: errormessage and wide/narrow style

--- src/components/FrameInput.vue
+++ src/components/FrameInput.vue
@@ -5,28 +5,49 @@
       {{ label }}
     </label>
     <div class="aktn-input--wrapper">
-      <div class="aktn-input--control">
+      <div class="aktn-input--control"
+        :class="{ 'aktn-input--control-wide': wide }">
         <slot name="control" />
       </div>
       <div v-if="hasActions" class="aktn-input--actions">
         <slot name="actions" />
       </div>
     </div>
+    <div v-if="error" class="aktn-input--error">
+      <AlertIcon :size="15" />
+      <slot name="error" />
+    </div>
   </div>
 </template>
 
 <script>
+
+import AlertIcon from 'vue-material-design-icons/AlertCircleOutline.vue'
 
 /**
  * Base component.
  */
 export default {
   name: 'FrameInput',
+
+  components: {
+    AlertIcon,
+  },
   props: {
     label: {
       type: String,
       required: false,
       default: null,
+    },
+    error: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    wide: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
   },
   computed: {
@@ -55,15 +76,29 @@ export default {
   &--wrapper {
     display: flex;
     gap: .5rem;
+    align-items: start;
   }
 
   &--control {
+    flex: 0;
+  }
+
+  &--control-wide {
     flex: 1;
   }
 
   &--actions {
     display: flex;
     flex-direction: row;
+  }
+
+  &--error {
+    display: flex;
+    color: var(--color-error);
+    font-size: 0.75rem;
+    align-content: baseline;
+    gap: .2rem;
+    margin-top: 4px;
   }
 
 }
